d543bb28938499dc67231b92ab71de687ef533ef
Grayfield Moningkey <grayfieldmm@gmail.com>
2019-02-20 17:12:25 +0700

    setup hierarchy configuration

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 const app = require('./routes');
+const config = require('../config');
 
-const port = 3000;
+const port = config.get('port') * 1 || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
